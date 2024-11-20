0eeae192c96943e1463f0ff92a5318dfaf6d4052
lgswin <glee6973@conestogac.on.ca>
2024-11-19 21:59:53 -0500

    update for frontend production

---
+++ workwise-01/backend/backend/build/precache-manifest.eade323dbcc58a1aaa9a57ff282d38f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e2f79e3cf6305b32c3993b4bed000f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c04511b44cfe98df1619",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e16a5b092c8e30ceff64",
+    "url": "/static/js/2.9421c5ca.chunk.js"
+  },
+  {
+    "revision": "da96256dd070ffac0081a3cd003f3cfd",
+    "url": "/static/js/2.9421c5ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44517888fae4c17e52f",
+    "url": "/static/js/3.bef7c3f4.chunk.js"
+  },
+  {
+    "revision": "c04511b44cfe98df1619",
+    "url": "/static/js/main.59b362c1.chunk.js"
+  },
+  {
+    "revision": "afb6367439fb39c7b4a3",
+    "url": "/static/js/runtime-main.d065833d.js"
+  }
+]);
